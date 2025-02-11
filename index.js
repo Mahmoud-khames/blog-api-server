ca367e68ecbe8cdfba04a28c9ec5283959507876
Mahmoud Khames Saber <158583024+Mahmoud-khames@users.noreply.github.com>
2025-02-11 14:53:08 +0200

    Update index.js

--- index.js
+++ index.js
@@ -11,8 +11,13 @@ import cors from "cors";
 
 const app = express();
 const port = process.env.PORT || 3001;
-app.use(cors({process.env.CLERK_FRONTEND_API,
-             credentials:true,}));
+app.use(cors(
+    {
+  origin: process.env.CLERK_FRONTEND_API,
+  // origin: " http://localhost:5173",
+   credentials:true, 
+}
+));
 app.use("/webhooks", webHookRouter);
 app.use(express.json());
 app.use(function (req, res, next) {
